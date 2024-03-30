dc7f650b0066d999f2508fb775020ee09c03bd71
Kevinxvii <kulejmanikevin@gmail.com>
2024-03-30 10:12:42 +0100

    fix buttons

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,6 @@
 /*qui ho fatto in modo che le card di tipo .col-md4 vengano temporaneamente rimosse dalla tabella se non rispettano i paramentri
 di ricerca. la ricerca parte in seguito al click del pulsante e mostra tutti gli sport che inziano con le determinate lettere inserite*/ 
 
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.querySelector('.form-control');
     const searchButton = document.getElementById('button-addon1');
