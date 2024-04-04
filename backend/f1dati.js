81318d9074f704a35eaf4824e7074330d4e40234
GiovanniFiliberti <redfiregiovik@gmail.com>
2024-04-04 21:37:27 +0200

    f1dati

--- backend/f1dati.js
+++ backend/f1dati.js
@@ -14,16 +14,37 @@ async function result() {
 
 
     
-  for (let i = 2000; i <= 2024; i++) {
+  for (let i = 1979; i <= 2024; i++) {
         
         const risposta = await fetch (`https://ergast.com/api/f1/${i}.json`)
         
         const seasons = await risposta.json()
         console.log(seasons)
         console.log(seasons.MRData.total)
-        
 
+        const ds = await fetch(`http://ergast.com/api/f1/${i}/driverStandings.json`,{
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+
+        let driverStandings = await ds.json()
+
+        await createOne(db,"Driver_Standings",driverStandings.MRData.StandingsTable)
+
+        const cs = await fetch(`http://ergast.com/api/f1/${i}/constructorStandings.json`,{
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+
+        let constructorStandings = await cs.json()
+
+        await createOne (db,"Constructor_Standings",constructorStandings.MRData.StandingsTable)
+        
+        
         for (let j = 1; j <= seasons.MRData.total; j++) {
+            
             const risposta = await fetch(`http://ergast.com/api/f1/${i}/${j}/results.json`, {
                 headers: {
                     'Accept': 'application/json'
@@ -31,13 +52,22 @@ async function result() {
             });
 
             let res = await risposta.json();
+            
             console.log(res.MRData.RaceTable);
-            //results.push(res);
-        
+            results.push(res);
+            
+            const q = await fetch(`http://ergast.com/api/f1/${i}/${j}/qualifying.json`)
+
+            let quali = await q.json();
+
+            console.log(quali.MRData.RaceTable)
 
             // Inserire ogni risultato nel DB
-            await createOne(db, "Result", res.MRData.RaceTable); 
+            await createOne(db, "Qualifying ", quali.MRData.RaceTable); 
+
+
         }
+        
     }
     // Opzionale: Stampa di verifica
     console.log("Dati inseriti con successo");
