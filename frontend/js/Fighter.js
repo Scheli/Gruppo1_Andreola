55880a72b4f8fd214660d135754dbb463563598e
Kevinxvii <kulejmanikevin@gmail.com>
2024-04-13 08:33:01 +0200

    ulteriori miglioramenti

--- frontend/js/Fighter.js
+++ frontend/js/Fighter.js
@@ -8,30 +8,25 @@ async function listaFighters() {
 
         const fighters = await response.json();
 
+        fighters.sort((a, b) => a.name.localeCompare(b.name));
+
         const container = document.getElementById('fighter-names');
 
         container.innerHTML = '';
 
-        // Create a <ul> element
         const ulElement = document.createElement('ul');
 
         fighters.forEach(fighter => {
-            // Create an <li> element for each fighter
             const liElement = document.createElement('li');
 
-            // Create an <a> element with href attribute set to '#' (you can replace '#' with the appropriate link)
             const aElement = document.createElement('a');
             aElement.textContent = fighter.name;
             aElement.href = '#'; // Replace '#' with the appropriate link
-
-            // Append the <a> element to the <li> element
             liElement.appendChild(aElement);
 
-            // Append the <li> element to the <ul> element
             ulElement.appendChild(liElement);
         });
 
-        // Append the <ul> element to the container
         container.appendChild(ulElement);
 
     } catch (error) {
